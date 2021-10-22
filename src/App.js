72c72fe0ece02998ee7c229a3e61f1ada23c4d6a
lukwiya23 <luk23bonnie8@gmail.com>
2021-10-22 12:10:27 +0300

    ||

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer'
 import Routes from './components/Routes';
 
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Hello quackqauck</h1>
+    <div className={darkTheme ? 'dark':''}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen ">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+        <Footer/>
+        </div>
     </div>
   );
 }
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Switch, Redirect, Route} from 'react-router-dom'
+import Results from './Results'
 
 const Routes = () => {
     return (
-        <div>
-            
+        <div className="p-4">
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to="/search"></Redirect>
+                </Route>
+                <Route path={['/search', '/images', '/news']}>
+                    <Results/>
+                </Route>
+            </Switch>
         </div>
     )
 }
